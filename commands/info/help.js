e84b524688e0cc5f4ee5cf85313eb97ff5ec3f3d
PrabhatCoderX <87633019+PrabhatCoderX@users.noreply.github.com>
2021-08-24 10:08:53 +0530

    Add files via upload

---
+++ commands/info/help.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color, Prefix } = require("../../config.js");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Help Command!",
+  usage: "Help | <Command Name>",
+  run: async(client, message, args) => {
+    
+    message.delete();
+    
+    let embed = new MessageEmbed()
+    .setColor(Color)
+    .setTitle(`${client.user.username} Commands!`)
+    .setDescription(`Use ${Prefix}help <Command Name> For More Command Information!` + 
+    "\n\n😉 **Fun**\n`avatar, coinflip, howgay, meme, rate, 8ball, ascii, choose, hack, randomnumber`" + "\n\n" + "⚙️ **Moderation**\n`clear, mute, unmute, tempmute, kick, ban, unban, tempban, warn, warnings, resetwarns`" + "\n\n"+
+    "📜 **Information**\n`help, weather, userinfo, serverinfo, ping`")
+    .setFooter(`Requested By ${message.author.username}`)
+    .setTimestamp();
+    
+    if (!args.length) return message.channel.send(embed);
+
+    let cmd =
+      client.commands.get(args[0].toLowerCase()) ||
+      client.commands.get(client.aliases.get(args[0].toLowerCase()));
+
+    let embed2 = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`${cmd.name} Information!`)
+      .addField(`Aliases`, cmd.aliases || "None!")
+      .addField(`Usage`, cmd.usage || "No Usage")
+      .addField(`Description`, cmd.description || "No Description!")
+      .setTimestamp();
+
+    if (cmd) {
+      return message.channel.send(embed2);
+    } else {
+      return message.channel.send(embed);
+    }
+  }
+};
---
+++ config.js
@@ -0,0 +1,3 @@
+exports.Prefix = `!`;
+exports.Token = `ODc2Njg2OTY5NDk1NzQ4NjI4.YRnsXQ.vH0YQWjyPTfjJMr8paBOT8jmkNo`;
+exports.Color = `RANDOM`;
